12e8f0a8345d918357329e60e01b77a8d92c5750
isaac <eoisaacc@gmail.com>
2022-02-20 22:24:41 -0300

    Add favicon and start js idea

--- src/js/script.js
+++ src/js/script.js
@@ -22,4 +22,38 @@ const updateCurrentYear = () => {
 
 copyrightYear.textContent = updateCurrentYear();
 
+// GAME ========================================================================
+const interface =  {
+	score: document.querySelector('[data-js="game-score"]'),
+	time: document.querySelector('[data-js="game-time"]'),
+	word: document.querySelector('[data-js="game-word"]'),
+}
+
+let gameScore = 0;
+
+const currentWord = 'sapato';
+
+const userWord = [];
+
+window.addEventListener('keydown', (event) => {
+	let key = event.key;
+
+	userWord.push(`${key}`);
+
+	console.log(userWord);
+
+	let teste = userWord.join('');
+
+	interface.word.textContent = teste;
+	if(teste === currentWord) {
+		console.log('CORRETO');
+		++gameScore;
+		interface.score.textContent = leftZero(gameScore);
+	}
+});
+
+const leftZero = (value) => {
+	return value < 10 ? `0${value}` : value;
+}
+
 
