ae5e008b161deb4fd4c6755a332a96bb511f29cf
isaac <eoisaacc@gmail.com>
2022-02-21 13:19:58 -0300

    Create homescreen and game countdown

--- src/js/script.js
+++ src/js/script.js
@@ -10,50 +10,93 @@ const copyrightYear = document.querySelector('[data-js="copyright-year"]');
 
 const updateCurrentYear = () => {
   const currentYear = new Date().getFullYear();
-	const yearLocalStorage = getItemOfLocalStorage('year');
+  const yearLocalStorage = getItemOfLocalStorage('year');
 
-	if(yearLocalStorage && yearLocalStorage === currentYear) {
-		return yearLocalStorage;
-	} else {
-		setItemOnLocalStorage('year', currentYear);
-		return getItemOfLocalStorage('year');
-	}
+  if (yearLocalStorage && yearLocalStorage === currentYear) {
+    return yearLocalStorage;
+  } else {
+    setItemOnLocalStorage('year', currentYear);
+    return getItemOfLocalStorage('year');
+  }
 };
 
 copyrightYear.textContent = updateCurrentYear();
 
 // GAME ========================================================================
-const interface =  {
-	score: document.querySelector('[data-js="game-score"]'),
-	time: document.querySelector('[data-js="game-time"]'),
-	word: document.querySelector('[data-js="game-word"]'),
-}
+const interface = {
+  countdown: document.querySelector('[data-js="homescreen-countdown"]'),
+  homescreen: document.querySelector('[data-js="homescreen"]'),
+  playBtn: document.querySelector('[data-js="play-btn"]'),
+  score: document.querySelector('[data-js="game-score"]'),
+  time: document.querySelector('[data-js="game-time"]'),
+  word: document.querySelector('[data-js="game-word"]'),
+};
 
 let gameScore = 0;
+let gameIsRunning = false;
 
-const currentWord = 'sapato';
+const words = [
+  'Idade',
+  'Aposentado',
+  'Zona',
+  'Jogar',
+  'Pacote',
+  'Ninho',
+  'Comida',
+  'Farinha',
+  'Dinamite',
+  'Banana',
+];
 
-const userWord = [];
-
-window.addEventListener('keydown', (event) => {
-	let key = event.key;
-
-	userWord.push(`${key}`);
-
-	console.log(userWord);
-
-	let teste = userWord.join('');
-
-	interface.word.textContent = teste;
-	if(teste === currentWord) {
-		console.log('CORRETO');
-		++gameScore;
-		interface.score.textContent = leftZero(gameScore);
-	}
-});
+const toggleHideElements = (elements) => {
+  elements.forEach((element) => element.classList.toggle('hidden-display'));
+};
 
 const leftZero = (value) => {
-	return value < 10 ? `0${value}` : value;
-}
+  return value < 10 ? `0${value}` : value;
+};
 
+const homescreenCountdown = () => {
+  let countdownTime = 5;
+  const timer = setInterval(() => {
+    interface.countdown.textContent = countdownTime;
+    --countdownTime;
+    if (countdownTime === -1) {
+      clearInterval(timer);
+      toggleHideElements([interface.homescreen]);
+      startGame();
+    }
+  }, 1000);
+};
 
+const gameCountdown = (time = 60) => {
+  const timer = setInterval(() => {
+    interface.time.textContent = leftZero(time);
+    --time;
+
+    if (time === -1) {
+      clearInterval(timer);
+      stopGame();
+    }
+  }, 1000);
+};
+
+const startGame = () => {
+  gameIsRunning = true;
+  gameCountdown();
+};
+
+const stopGame = () => {
+  gameIsRunning = false;
+  toggleHideElements([
+    interface.playBtn,
+    interface.homescreen,
+    interface.countdown,
+  ]);
+  console.log('The game is stoped');
+};
+
+interface.playBtn.addEventListener('click', () => {
+  toggleHideElements([interface.playBtn, interface.countdown]);
+  homescreenCountdown();
+});
